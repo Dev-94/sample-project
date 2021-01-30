4b0a373cdc575fff188019b2beae9eecf59edf0d
Dev Govindji <friendsofdev94@gmail.com>
2021-01-30 14:17:42 -0600

    about to add services, after creating two components

---
+++ src/app/mock-courses.ts
@@ -0,0 +1,31 @@
+import { Course } from './course';
+export const COURSES: Course[] = [
+    {
+        id: 1,
+        name: "C++"
+    },
+    {
+        id: 2,
+        name: "JavaScript"
+    },
+    {
+        id: 3,
+        name: "Python"
+    },
+    {
+        id: 4,
+        name: "C#"
+    },
+    {
+        id: 5,
+        name: "React"
+    },
+    {
+        id: 6,
+        name: "Vue"
+    },
+    {
+        id: 7,
+        name: "GraphQL"
+    },
+]
